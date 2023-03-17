9a5d00688e4aed6685e6d5110704fffc8435b11e
github-dns147 <rarden@ya.ru>
2023-03-17 22:23:41 +0300

    feat: add react-router home page

--- react-components/src/main.tsx
+++ react-components/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Root from "./routes/root";
 import App from './App';
-import './index.css';
+import './index.scss';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ react-components/src/routes/root.tsx
@@ -0,0 +1,14 @@
+export default function Root() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href={`/about`}>About Us</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
