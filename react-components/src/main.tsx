3badf34c55374c182e0fed4e4596043a1a23e750
github-dns147 <rarden@ya.ru>
2023-03-17 22:42:03 +0300

    feat: add error page

--- react-components/src/main.tsx
+++ react-components/src/main.tsx
@@ -4,14 +4,16 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
-import Root from "./routes/root";
-import App from './App';
 import './index.scss';
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
