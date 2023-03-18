1424d7594776f90b15da1fa1a4de46c6856673b0
github-dns147 <rarden@ya.ru>
2023-03-18 21:16:51 +0300

    feat: add footer

---
+++ react-components/src/routes/footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <div id="footer">
+      <ul className="footer-list">
+        <li className="footer-item">
+          <img src={"https://svgshare.com/i/og2.svg"} width="40" alt="icon" />
+          <a href="https://github.com/dns147" target="_blank">dns147</a>
+        </li>
+        <li className="footer-item">
+          <span>2023</span>
+        </li>
+        <li className="footer-item">
+          <a href="https://rs.school/js/" target="_blank">
+            <img src={"https://rs.school/images/rs_school_js.svg"} width="70" alt="icon" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- react-components/src/routes/root.tsx
+++ react-components/src/routes/root.tsx
@@ -1,10 +1,12 @@
 import Header from "./header";
 import { Outlet } from "react-router-dom";
+import Footer from "./footer";
 
 export default function Root() {
   return (
     <>
       <Header />
+      <Footer />
       <Outlet />
     </>
   );
