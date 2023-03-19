5703ee8e3ec12e211ca358c367458cf61d798847
github-dns147 <rarden@ya.ru>
2023-03-19 10:00:22 +0300

    feat: add search bar

--- react-components/src/routes/home-page.tsx
+++ react-components/src/routes/home-page.tsx
@@ -1,7 +1,33 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
 export default function HomePage() {
+  const localDataValue = localStorage.getItem('inputValue');
+  const [valueInput, setValueInput] = useState(localDataValue ?? '');
+ 
+  function handleSubmit(event: FormEvent): void {
+    event.preventDefault();
+    localStorage.setItem('inputValue', valueInput);
+  }
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    setValueInput(event.target.value);
+  }
+
   return (
     <>
       <h1>Home Page</h1>
+      <form id="search-form" role="search" onSubmit={handleSubmit}>
+        <button type="submit"></button>
+        <input
+          id="q"
+          aria-label="Search field"
+          placeholder="Search"
+          type="search"
+          name="q"
+          value={valueInput}
+          onChange={handleChange}
+        />
+      </form>
     </>
   );
 }
