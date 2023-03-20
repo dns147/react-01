fec8796fc08551fb72176eed6c3cff83eb4af8aa
github-dns147 <rarden@ya.ru>
2023-03-20 22:11:56 +0300

    feat: add name page on header

--- react-components/src/routes/header.tsx
+++ react-components/src/routes/header.tsx
@@ -1,10 +1,32 @@
+import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default function Header() {
-  return (
-    <nav id="nav-menu">
-      <NavLink to="/home">Home</NavLink>
-      <NavLink to="/about">About Us</NavLink>
-    </nav>
-  );
+type MyProps = {};
+type MyState = { hash: string };
+
+export default class Header extends Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props);
+
+    this.state = {
+      hash: '',
+    };
+  }
+
+  updateHash(): string {
+    this.setState({ hash: window.location.pathname.slice(1) });
+    return this.state.hash;
+  }
+
+  render() {
+    return (
+      <>
+        <nav id="nav-menu">
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/about">About Us</NavLink>
+          <b>{this.updateHash() + ' page'}</b>
+        </nav>
+      </>
+    );
+  }
 }
