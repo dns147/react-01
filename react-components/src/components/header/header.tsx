56b31e1b86f819a4b327463e9937b6e6734551f2
github-dns147 <rarden@ya.ru>
2023-04-01 14:18:24 +0300

    refactor: header component

--- react-components/src/components/header/header.tsx
+++ react-components/src/components/header/header.tsx
@@ -1,44 +1,28 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-type MyProps = {};
-type MyState = {
-  location: string;
-};
+export default function Header() {
+  const [currentLocation, setCurrentLocation] = useState('home');
 
-export default class Header extends Component<MyProps, MyState> {
-  constructor(props: MyProps) {
-    super(props);
-
-    this.state = {
-      location: location.pathname.slice(1),
-    };
-  }
-
-  handleClick = (): void => {
-    const currentPathname: string = location.pathname.slice(1);
-    this.setState({ location: currentPathname });
+  const handleClick = (): void => {
+    const currentPathname: string = window.location.pathname.slice(1);
+    setCurrentLocation(currentPathname);
   };
 
-  render() {
-    const { location } = this.state;
-    const firstPage: string = location ? ' page' : 'home page';
-
-    return (
-      <>
-        <nav id="nav-menu" onClick={this.handleClick}>
-          <NavLink className={'nav-home-page'} to="/home">
-            Home
-          </NavLink>
-          <NavLink className={'nav-forms-page'} to="/forms">
-            Forms
-          </NavLink>
-          <NavLink className={'nav-about-page'} to="/about">
-            About Us
-          </NavLink>
-          <b className={'current-page'}>{location + firstPage}</b>
-        </nav>
-      </>
-    );
-  }
+  return (
+    <>
+      <nav id="nav-menu" onClick={handleClick}>
+        <NavLink className={'nav-home-page'} to="/home">
+          Home
+        </NavLink>
+        <NavLink className={'nav-forms-page'} to="/forms">
+          Forms
+        </NavLink>
+        <NavLink className={'nav-about-page'} to="/about">
+          About Us
+        </NavLink>
+        <b className={'current-page'}>{currentLocation + ' page'}</b>
+      </nav>
+    </>
+  );
 }
